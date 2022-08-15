f7f769dfa0ab0b76000bbeb569fe6937ad05b7e2
Khoshfekr MH <khoshfekr2001@protonmail.com>
2022-08-16 00:34:29 +0430

    Data function created to show today data at top of To Do List page

---
+++ data.js
@@ -0,0 +1,21 @@
+exports.getData = function () {
+    const today = new Date();
+
+    const option = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    }
+
+    return today.toLocaleDateString("en-US", option);
+}
+
+exports.getDay = function () {
+    const today = new Date();
+
+    const option = {
+      weekday: "long"
+    };
+
+    return today.toLocaleDateString("en-US", option);
+}
