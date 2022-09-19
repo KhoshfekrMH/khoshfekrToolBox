98c78b5cea27785fe072398e800d7bfe2cedde7e
Khoshfekr MH <97754590+KhoshfekrMH@users.noreply.github.com>
2022-09-19 19:02:57 +0430

    now our to-do-list page of Khoshfekr-toolbox connected to local mongoDB for now by mongoose

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const date = require(__dirname + "/data.js");
 const api = require(__dirname + "/apiKey.js");
 const {response} = require("express");
 const _ = require("lodash");
+const mongoose = require("mongoose");
 let posts = [];
 
 const app = express();
@@ -19,6 +20,8 @@ app.set('view engine', 'ejs');
 app.get("/", function (req,res) {
     res.sendFile(__dirname + "/index.html");
 });
+
+mongoose.connect("mongodb://localhost:27017/local-todoListDB");
 
 //#region BMI calculatorPage("/BMI")
 app.get("/BMI" , function (req,res) {
@@ -44,25 +47,116 @@ app.post("/BMI" , function (req,res) {
 //#endregion
 
 //#region TODOLIST page("/ToDoList")
-const items = ["buy food", "cook food", "eat food"];
+const itemsSchema = {
+    name: String
+};
+const Item = mongoose.model("item", itemsSchema);
+
+const item1 = new Item({
+    name: "Welcome to your todolist!"
+})
+
+const item2 = new Item({
+    name: "Hit the + button to add a new item."
+})
+
+const item3 = new Item({
+    name: "<-- Hit this to delete an item."
+})
+
+const defaultItems = [item1, item2, item3];
+
+const listSchema = {
+    name: String,
+    items: [itemsSchema]
+};
+
+const List = mongoose.model("List", listSchema);
+
 app.get("/ToDoList",function (req,res) {
-    const dayTime = date.getData();
-   res.render("toDoList" , {
-       homeNavActive: "",
-       BMINavActive: "",
-       toDoListNavActive: "active",
-       weatherNavActive: "",
-       signUpNavActive: "",
-       blogNavActive: "",
-       listTitle: dayTime,
-       newListItems: items
+
+    Item.find({}, function(err, foundItems){
+
+        if (foundItemslength === 0) {
+            Item.insertMany(defaultItems, function(err){
+                if (err) {
+                    console.log(err);
+                } else {
+                    console.log("Successfully savevd default items to DB.");
+                }
+            });
+            res.redirect("/");
+        } else {
+            res.render("toDoList" , {
+                homeNavActive: "",
+                BMINavActive: "",
+                toDoListNavActive: "active",
+                weatherNavActive: "",
+                signUpNavActive: "",
+                blogNavActive: "",
+                listTitle: "Today",
+                newListItems: foundItems
+            });
+        }
+    });
+});
+
+app.get("/ToDoList/:customListName", function (req, res) {
+   const customListName = _.capitalize(req.params.customListName);
+
+   List.findOne({name: customListName}, function (err, foundList) {
+      if(!err) {
+          if(!foundList) {
+              const list = new List({
+                 name: customListName,
+                 items: defaultItems
+              });
+              list.save();
+              res.redirect("/ToDoList/" + customListName);
+          } else {
+              res.render("list", {listTitle: foundList.name, newListItems: foundList.items});
+          }
+      }
    });
 });
 
 app.post("/ToDoList", function (req,res) {
-    const item = req.body.newItem;
-    items.push(item);
-    res.redirect("/ToDoList");
+    const itemName = req.body.newItem;
+    const listName = req.body.list;
+
+    const item = new Item ({
+        name: itemName
+    });
+
+    if(listName === "Today"){
+        item.save();
+    } else {
+        List.findOne({name: listName}, function (err, foundList) {
+           foundList.items.push(item);
+           foundList.save();
+           res.redirect("/ToDoList/" + listName);
+        });
+    }
+});
+
+app.post("/ToDoList/delete", function (req,res) {
+    const checkedItemId = req.body.checkbox;
+    const listName = req.body.listName;
+    
+    if (listName === "Today")   {
+        Item.findByIdAndRemove(checkedItemId, function (err) {
+            if(!err) {
+                console.log("Successfully deleted checked item.");
+                res.redirect("/ToDoList");
+            }
+        })
+    } else {
+        List.findOneAndUpdate({name: listName}, {$pull: {items: {_id: checkedItemId}}}, function (err, foundList) {
+            if(!err) {
+                res.redirect("/ToDoList" + listName)
+            }
+        });
+    }
 });
 //#endregion
 
